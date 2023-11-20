185c1204079fd99e11d6968fe9af3e183df768d1
matuzalembatista <matuzalem_pereira@hotmail.com>
2023-11-20 12:53:41 -0300

    Criando classe aluno com Get/Set

---
+++ src/Aluno.java
@@ -0,0 +1,29 @@
+public class Aluno {
+    private String nome;
+    private double nota;
+
+    public Aluno(String nome, double nota) {
+        this.nome = nome;
+        this.nota = nota;
+    }
+    public Aluno(){
+
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public double getNota(){
+        return nota;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public void setNota(double nota){
+        this.nota = nota;
+    }
+
+}
